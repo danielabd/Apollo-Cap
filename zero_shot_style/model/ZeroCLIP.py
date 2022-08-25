a6c4780af74a9c7de04979437f6774af61dc4a04
Daniela Ben-David <danielabendavid@Danielas-MacBook-Pro.local>
2022-08-26 01:41:06 +0300

    fix

--- zero_shot_style/model/ZeroCLIP.py
+++ zero_shot_style/model/ZeroCLIP.py
@@ -535,7 +535,7 @@ class CLIPTextGenerator:
         while(1):
             i=i+1
             print(f' iteration num = {i}')
-            if i>6:
+            if i>3:
                 break
             curr_shift = [tuple([torch.from_numpy(x).requires_grad_(True).to(device=self.device) for x in p_]) for p_ in
                           context_delta]
