fix bug precision

zero_shot_style/evaluation/text_style_classification.py
@@ -343,7 +343,7 @@ def train(model, optimizer, df_train, df_val, labels_set_dict, labels_idx_to_str
 
         train_preds_t = torch.tensor(train_preds)
         train_targets_t = torch.tensor(train_targets)
-        precision_i = Precision(average='weighted', num_classes=len(set(np.array(train_targets_t))))
+        precision_i = Precision(average='weighted', task='binary',  num_classes=len(set(np.array(train_targets_t))))
         precision = precision_i(train_preds_t, train_targets_t)
         recall_i = Recall(average='weighted', num_classes=len(set(np.array(train_targets_t))))
         recall = recall_i(train_preds_t, train_targets_t)
@@ -384,7 +384,7 @@ def train(model, optimizer, df_train, df_val, labels_set_dict, labels_idx_to_str
             preds_t = torch.tensor(preds)
             targets_t = torch.tensor(val_targets)
 
-            precision_i = Precision(average='weighted', num_classes=len(set(np.array(targets_t))))
+            precision_i = Precision(average='weighted', task='binary', num_classes=len(set(np.array(targets_t))))
             precision = precision_i(preds_t, targets_t)
             recall_i = Recall(average='weighted', num_classes=len(set(np.array(targets_t))))
             recall = recall_i(preds_t, targets_t)