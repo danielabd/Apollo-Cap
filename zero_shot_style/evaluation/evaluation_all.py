5be6aefd24e502f7f873a7ca873f70a859f1a667
Daniela Ben-David <bendavid.daniela@gmail.com>
2023-09-28 23:56:14 +0300

    to know which test

--- zero_shot_style/evaluation/evaluation_all.py
+++ zero_shot_style/evaluation/evaluation_all.py
@@ -1463,9 +1463,9 @@ def main():
 ,577169]
     current = list(res_data_per_test['audio'].keys())
     comp = []
-    for i in desired:
-        if i not in current:
-            comp.append(i)
+    for i in current:
+        if i not in desired:
+            # comp.append(i)
             res_data_per_test['audio'].remove(i)
     print(f"run over {len(list(res_data_per_test['audio'].keys()))} images.")
     print(f"imgs are:{list(res_data_per_test['audio'].keys())}")
